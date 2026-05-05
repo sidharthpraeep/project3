e5018b3feef8ed3123c38062091317b1fe2488b0
sidharthpradeep <sidharth19nov2004@gmail.com>
2026-05-05 19:43:44 +0530

    main

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "project3"));
+  res.sendFile(path.join(__dirname, "public/"));
 });
 
 app.listen(3000, () => {
